5259255ab02dbe64b1433c872f006faf452805c9
joseycartagena <jose.cartagena.c@gmail.com>
2023-11-19 23:13:49 -0300

    Commit inicial

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+precio = 400000
+
+precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio
+
+valor = 0
+
+valorSpan = document.querySelector(".valor-total");
+valorSpan.innerHTML = valor
